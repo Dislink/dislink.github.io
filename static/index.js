fd485cecd3dbe94854d0734027ae3df12cb0aef1
Dislink <51643958+Dislink@users.noreply.github.com>
2022-01-26 18:43:24 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -401,10 +401,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         }
         // TODO 添加反馈
         if (mode === MODE_ENDLESS) return '';
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
+        if (score <= 49) return '一锹除走！';
+        if (score <= 99) return '道德败坏！';
+        if (score <= 149) return '浑蛋!';
+        if (score <= 199) return '分享关键词？';
         return '人？';
     }
 
